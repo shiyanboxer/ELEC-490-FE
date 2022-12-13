4c2150772674d88579741ca63eb507b56e9aab6e
Shiyan Boxer <shiyanboxer7@gmail.com>
2022-12-13 15:40:46 -0500

    update dummy values

--- src/sections/@dashboard/app/CardData.js
+++ src/sections/@dashboard/app/CardData.js
@@ -1,25 +1,25 @@
 export default [
     {
         title:'Recovery Score',
-        total: 123,
+        total: 81,
         // color: 'info',
         icon:'material-symbols:health-and-safety',
     },
     {
-        title:'Average Heart Rate Variability',
-        total: 1352831,
+        title:'Average Heart Rate Variability (ms)',
+        total: 828,
         color: 'info',
         icon: 'uil:heart-rate',
     },
     {
-        title: 'Weekly Training Hours',
-        total: 678,
+        title: 'Weekly Training (Hours)',
+        total: 23,
         color: 'warning',
         icon: 'ic:outline-directions-bike',
     },
     {
-        title: 'Heart Rate',
-        total: 890,
+        title: 'Heart Rate (BPM)',
+        total: 120,
         color: 'error',
         icon: 'ant-design:heart-filled'
     }
