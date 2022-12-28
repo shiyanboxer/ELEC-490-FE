27af16b54346364ebb2ab9bdb407886067a3f881
Shiyan Boxer <shiyanboxer7@gmail.com>
2022-12-28 18:42:45 -0500

    Adding hook for passing HRV value

--- src/sections/@dashboard/app/FormDialog.js
+++ src/sections/@dashboard/app/FormDialog.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {useState} from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
@@ -6,21 +7,51 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import { Typography } from '@mui/material';
 
 export default function FormDialog() {
   const [open, setOpen] = React.useState(false);
+  const [hrv, setHrv] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleClickOpen = () => {
     setOpen(true);
-  };
+    setMessage("");
 
+  };
   const handleClose = () => {
     setOpen(false);
   };
 
+  // https://www.techomoro.com/submit-a-form-data-to-rest-api-in-a-react-app/
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("HWERWER")
+    console.log(e)
+    try {
+      const response = await fetch("https://dummyendpoint", {
+        method: "POST",
+        body: JSON.stringify({
+          hrv,
+        }),
+      });
+      const responseJSON = await response.json();
+      if (response.status === 200) {
+        console.log("Successful")
+        setHrv("");
+        setMessage("HRV value successfully submitted");
+        setOpen(false);
+      } else {
+        setMessage("An error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
-      <Button fullWidth style={{marginBottom: "5%", marginTop: "3%"}} variant="contained" size="large" onClick={handleClickOpen}>
+      <Button fullWidth style={{marginBottom: "2%", marginTop: "3%"}} variant="contained" size="large" onClick={handleClickOpen}>
         Heart Rate Variability Value
       </Button>
 
@@ -39,13 +70,16 @@ export default function FormDialog() {
             variant="standard"
             type="number"
             required="true"
+            onChange={(e) => setHrv(e.target.value)}
           />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} >Cancel</Button>
-          <Button onClick={handleClose}>Submit</Button>
+          <Button  onClick={handleSubmit}>Submit</Button>
         </DialogActions>
       </Dialog>
+
+      <Typography variant="subtitle1" color="green" style={{marginBottom: "2%", marginTop: "1%"}}>{message ? <p>{message}</p> : null}</Typography>
       
     </div>
   );
