a1982e50cf050f76769746385f1df0f0584b752a
Shiyan Boxer <shiyanboxer7@gmail.com>
2023-02-03 13:32:03 -0500

    Update prod

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -148,11 +148,10 @@ export default function DashboardAppPage() {
           <Grid item xs={12} md={6} lg={4}>
             <AppCurrentSubject
               title="Workout Breakdown"
-              chartLabels={['Endurance', 'Interval', 'Threshold', 'Hill']}
+              chartLabels={['Anaerobic Capacity', 'VO2 Max', 'Threshold', 'Tempo', 'Endurance', 'Recover']}
               chartData={[
-                { name: 'Series 1', data: [80, 50, 30, 40, 100, 20] },
-                { name: 'Series 2', data: [20, 30, 40, 80, 20, 80] },
-                { name: 'Series 3', data: [44, 76, 78, 13, 43, 10] },
+                { name: 'Power', data: [80, 50, 30, 40, 100, 20] },
+                { name: 'Heart Rate', data: [20, 30, 40, 80, 20, 80] },
               ]}
               chartColors={[...Array(6)].map(() => theme.palette.text.secondary)}
             />
