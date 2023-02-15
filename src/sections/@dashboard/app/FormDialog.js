6ce886710fc3061d6eab551c642d1ba9bf3426ec
Shiyan Boxer <shiyanboxer7@gmail.com>
2023-02-15 12:55:16 -0500

    Update to PythonAnywhere

--- src/sections/@dashboard/app/FormDialog.js
+++ src/sections/@dashboard/app/FormDialog.js
@@ -32,7 +32,8 @@ export default function FormDialog() {
     console.log(e)
     try {
       // const response = await fetch("http://127.0.0.1:5000/predict", {
-      const response = await fetch("https://elec49x.herokuapp.com/predict", {
+      // const response = await fetch("https://elec49x.herokuapp.com/predict", {
+      const response = await fetch("http://shiyanboxer.pythonanywhere.com/predict", {
         method: "POST",
         body: JSON.stringify({
           hrv,
