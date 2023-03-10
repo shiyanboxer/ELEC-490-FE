d1edcb685d5a2a1e36830278186d9319e4645205
Shiyan Boxer <shiyanboxer7@gmail.com>
2023-03-09 19:03:21 -0500

    update onboarding link

--- src/sections/auth/login/SingupForm.js
+++ src/sections/auth/login/SingupForm.js
@@ -17,8 +17,8 @@ export default function SignupForm() {
     // navigate('/onboarding', { replace: true });
     // const url = 'http://127.0.0.1:5000';
     // const url = 'https://elec49x.herokuapp.com/';
-    const url = 'http://localhost:3000/onboarding';
-    // const url = 'https://elec49x.netlify.app/onboarding'
+    // const url = 'http://localhost:3000/onboarding';
+    const url = 'https://elec49x.netlify.app/onboarding'
     window.location.href = url;
   };
 
